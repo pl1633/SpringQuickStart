46ef7dc55496206884b141bab6d214b1b2c85cd9
DESKTOP-RULLE8I\학생 <pl1633@naver.com>
2023-08-03 09:19:56 +0900

    no message

--- src/test/java/com/springbook/biz/board/BoardServiceClient.java
+++ src/test/java/com/springbook/biz/board/BoardServiceClient.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class BoardServiceClient {
 	
 	public static void main(String[] args) {
